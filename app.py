3ba70d98b84d5b7bb9794c1454e6e6704abd1a44
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-09-29 20:07:26 -0700

    update app and requirements.txt

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 
 specs = pd.read_csv("./data/product_specs.csv")
 
@@ -38,38 +39,41 @@ app.layout = [
         color="primary",
         dark=True
     ),
+    dbc.Container(
+        class_name="m-0 p-0",
+        children=[dbc.Row(
+            class_name="mt-2",
+            children=[
+                dbc.Col(
+                    width=2,
+                    class_name="bg-light",
+                    style={"minHeight":"95vh"},
+                    children=[
+                        html.H4("Specs", className="ms-4"),
+                        dbc.RadioItems(
+                            id="spec_name",
+                            class_name="p-0",
+                            options=[{"label": x if x.isupper() else x.replace("_", " ").capitalize(), "value": x} for x
+                                     in
+                                     units_dict.keys()],
+                            value="starting_price",  # Default selected value
+                            inline=False,  # Stacks vertically
+                            label_class_name="btn rounded-0 w-100",
+                            label_checked_class_name="bg-primary text-white",
+                            input_class_name="d-none",
+                            label_style={"textAlign": "left"}
+                        ),
+                    ]
+                ),
+                dbc.Col(
+                    width=10,
+                    children=[
+                        dcc.Graph(id='graph-content')
+                    ]
+                )
 
-    dbc.Row(
-        class_name="mt-2",
-        children=[
-            dbc.Col(
-                width=2,
-                children=[
-                    html.H3("Specs"),
-                    dbc.RadioItems(
-                        id="spec_name",
-                        class_name="p-0",
-                        options=[{"label": x if x.isupper() else x.replace("_", " ").capitalize(), "value": x} for x
-                                 in
-                                 units_dict.keys()],
-                        value="starting_price",  # Default selected value
-                        inline=False,  # Stacks vertically
-                        label_class_name="btn rounded-0 w-100",
-                        label_checked_class_name="bg-primary text-white",
-                        input_class_name="d-none",
-                        label_style={"textAlign": "left"}
-                    ),
-                ]
-            ),
-            dbc.Col(
-                width=10,
-                children=[
-                    dcc.Graph(id='graph-content')
-                ]
-            )
-
-        ]
-    )
+            ]
+        )])
 
 ]
 
@@ -80,9 +84,20 @@ app.layout = [
     Input('spec_name', 'value')
 )
 def update_graph(value):
-    dff = specs[['product_id', 'product_name', value]]
+    if value == "FOV":
+        df_melted = specs.melt(id_vars=['product_id', 'product_name'], value_vars=['FOV_vertical', 'FOV_horizontal'],
+                               var_name='FOV', value_name='Value')
 
-    return px.bar(dff, x='product_name', y=value)
+        fig = px.bar(df_melted, x='product_name', y='Value', color='FOV',
+                      barmode='group')
+        fig.update_layout(plot_bgcolor ="white")
+
+        return fig
+
+    df = specs[['product_id', 'product_name', value]]
+    fig= px.bar(df, x='product_name', y=value)
+    fig.update_layout(plot_bgcolor="white")
+    return fig
 
 
 if __name__ == '__main__':
