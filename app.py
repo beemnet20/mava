e1c33994d570c64f9565207910ab28868dd63015
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-09-29 19:01:57 -0700

    add sidebar style input

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
-import numpy as np
 import dash_bootstrap_components as dbc
 
 specs = pd.read_csv("./data/product_specs.csv")
-
 
 units_dict = dict(starting_price="dollars", DRAM="GB",
                   base_storage="GB", display_resolution_PPI="Pixels per inch",
                   display_resolution_PPD="pixels per degree",
                   refresh_rate="Hz", color_gamut="% of sRGB",
-                  battery_run_time = "hours",
-                  FOV_vertical = "degrees",
-                  FOV_horizontal = "degrees"
+                  battery_run_time="hours",
+                  FOV="degrees"
                   )
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -28,31 +25,62 @@ app.layout = [
                 dbc.Row(
                     [
                         dbc.Col(html.Img(src="/assets/images/logo.svg", height="30px")),
-                        dbc.Col(dbc.NavbarBrand("MAVA", className="ms-2")),
+                        dbc.Col(dbc.NavbarBrand("MAVA", class_name="ms-2")),
                     ],
                     align="center",
-                    className="g-0",
+                    class_name="g-0",
                 ),
                 href="#",
                 style={"textDecoration": "none"}
             )],
-            className="ms-2",
+            class_name="ms-2",
         )],
         color="primary",
         dark=True
     ),
-    dcc.Dropdown(list(units_dict.keys()), "starting_price", id="dropdown-selection"),
-    dcc.Graph(id='graph-content')
+
+    dbc.Row(
+        class_name="mt-2",
+        children=[
+            dbc.Col(
+                width=2,
+                children=[
+                    html.H3("Specs"),
+                    dbc.RadioItems(
+                        id="spec_name",
+                        class_name="p-0",
+                        options=[{"label": x if x.isupper() else x.replace("_", " ").capitalize(), "value": x} for x
+                                 in
+                                 units_dict.keys()],
+                        value="starting_price",  # Default selected value
+                        inline=False,  # Stacks vertically
+                        label_class_name="btn rounded-0 w-100",
+                        label_checked_class_name="bg-primary text-white",
+                        input_class_name="d-none",
+                        label_style={"textAlign": "left"}
+                    ),
+                ]
+            ),
+            dbc.Col(
+                width=10,
+                children=[
+                    dcc.Graph(id='graph-content')
+                ]
+            )
+
+        ]
+    )
+
 ]
 
 
 @callback(
 
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('spec_name', 'value')
 )
 def update_graph(value):
-    dff = specs[['product_id','product_name',value]]
+    dff = specs[['product_id', 'product_name', value]]
 
     return px.bar(dff, x='product_name', y=value)
 
